a5bba6508566a474d3accb5c9b703a8e3c1edec6
john jorjill <orgil9506@gmail.com>
2020-02-28 11:56:17 +0800

    Setting environment variable

--- controllers/image.js
+++ controllers/image.js
@@ -1,4 +1,17 @@
+const Clarifai = require('clarifai');
 
+const app = new Clarifai.App({
+    apiKey: '565cfd7c28d54cb0b393980f346fe0cd'
+   });
+  
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL,req.body.input)
+        .then(data =>{
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work'))
+}
 
 const handleImage = (req,res,db) => {
     const { id } = req.body;
@@ -12,5 +25,6 @@ const handleImage = (req,res,db) => {
 }
 
 module.exports = {
-    handleImage
+    handleImage,
+    handleApiCall
 }
--- server.js
+++ server.js
@@ -35,7 +35,11 @@ app.post('/signin',(req,res)=>{ signin.handleSignin(req,res,db, bcrypt)})
 app.post('/register',(req,res)=>{ register.handleRegister(req,res,db,bcrypt) })
 app.get('/profile/:id',(req,res) => { profile.handleProfileGet(req,res,db)})
 app.put('/image',(req,res)=>{ image.handleImage(req,res,db)})
+app.post('/imageurl',(req,res)=>{ image.handleApiCall(req,res)})
 
-app.listen(3000, ()=>{
-    console.log('app is running');
-})
+const PORT = process.env.PORT
+app.listen(PORT, ()=>{
+    console.log(`app is running on port ${PORT}`);
+});
+
+console.log(PORT);
